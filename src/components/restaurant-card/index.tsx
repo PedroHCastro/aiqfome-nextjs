90ef81d937e70da6c7a16d840773d097967a26d1
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-09 22:19:14 -0300

    feat: add RestaurantCard component

---
+++ src/components/restaurant-card/index.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import styles from "./restaurant-card.module.css";
+import { RestaurantItem } from "@/types";
+
+interface Props {
+  data: RestaurantItem;
+  isOpen?: boolean;
+}
+
+export function RestaurantCard({ data, isOpen }: Props) {
+  const isFreeDelivery = data.delivery === 0;
+
+  const formatCurrency = (value: number) =>
+    value.toLocaleString("pt-BR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+  const getDeliveryIcon = () =>
+    isFreeDelivery ? "/delivery-icon.svg" : "/aiq-delivery-icon.svg";
+
+  const getDeliveryText = () =>
+    isFreeDelivery ? "grátis" : `R$${formatCurrency(data.delivery)}`;
+
+  return (
+    <div className={`${styles.card} ${isOpen && styles.cardIsOpen}`}>
+      <Image
+        src={`/${data.logo}`}
+        alt={`logo ${data.name}`}
+        width={72}
+        height={72}
+      />
+      <div className={styles.infoWrapper}>
+        <h3 className={styles.title}>{data.name}</h3>
+        <div className={styles.metadataWrapper}>
+          <div className={styles.metadataItem}>
+            <Image
+              src={getDeliveryIcon()}
+              alt="Ícone de entrega"
+              width={18}
+              height={18}
+              aria-hidden="true"
+            />
+            <span
+              className={isFreeDelivery ? styles.deliveryFree : styles.delivery}
+            >
+              {getDeliveryText()}
+            </span>
+          </div>
+
+          <span className={styles.detail}>•</span>
+
+          <div className={styles.metadataItem}>
+            <Image
+              src="/rating-icon.svg"
+              alt="icone de avaliação"
+              width={18}
+              height={18}
+            />
+            <span className={styles.rating}>{data.rating}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
