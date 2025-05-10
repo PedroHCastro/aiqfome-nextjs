563d112b687361676d277ba68d6ed3f7aff5ad80
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-09 22:22:34 -0300

    feat: add RestaurantsList in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import RestaurantsList from "@/components/restaurants-list";
 export default function Home() {
   return (
     <div>
       <main>
         <h2>abertos</h2>
+        <RestaurantsList />
       </main>
     </div>
   );
