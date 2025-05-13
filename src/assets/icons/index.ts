1f58effa2d863e4f20870d8b206b29b86302978d
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 06:59:57 -0300

    feat: add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,10 +3,18 @@ import ChevronDownIcon from "./chevron-down-icon.svg";
 import DiscountIcon from "./discount-icon.svg";
 import HotIcon from "./hot-icon.svg";
 import VegIcon from "./veg-icon.svg";
+import DeliveryIcon from "./delivery-icon.svg";
+import HeartIcon from "./heart-icon.svg";
+import ShareIcon from "./share-icon.svg";
+import RatingIcon from "./rating-icon.svg";
 export {
   ChevronRightIcon,
   ChevronDownIcon,
   DiscountIcon,
   HotIcon,
   VegIcon,
+  DeliveryIcon,
+  HeartIcon,
+  ShareIcon,
+  RatingIcon,
 };
