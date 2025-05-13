a92300ffd7f9502bd60ac3ffc4621df52c0911be
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 07:29:11 -0300

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import RestaurantsList from "@/components/restaurants-list";
+import Image from "next/image";
+import { RestaurantsList } from "@/components/home";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Image
+        src="/homeBanner.png"
+        width={0}
+        height={0}
+        alt="Banner promicional"
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+          marginTop: 1,
+        }}
+      />
       <main className={styles.main}>
         <RestaurantsList />
       </main>
