37ecbe6b023502e174610cd06c0c665519dbd06f
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 06:21:57 -0300

    feat: add product card  and product list components

---
+++ src/components/catalogo/product-card/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { DiscountIcon, HotIcon, VegIcon } from "@/assets/icons";
+import { MenuItem } from "@/types/products";
+
+import styles from "./product-card.module.css";
+
+interface Props {
+  data: MenuItem;
+}
+
+export function ProductCard({ data }: Props) {
+  const formatPrice = (price: number) =>
+    `R$ ${price.toFixed(2).replace(".", ",")}`;
+
+  const pathname = usePathname();
+
+  return (
+    <Link href={`${pathname}/${data.slug}`}>
+      <div key={data.slug} className={styles.contentItem}>
+        <div className={styles.itemInfoWrapper}>
+          <div className={styles.itemTitleWrapper}>
+            <p className={styles.itemTitle}>{data.name}</p>
+            {data.icon === "veg" && <VegIcon />}
+            {data.icon === "hot" && <HotIcon />}
+          </div>
+          <p className={styles.itemDescription}>{data.description}</p>
+        </div>
+
+        <div className={styles.itemPriceWrapper}>
+          {data.startingAt && (
+            <span className={styles.startingAt}>a partir de</span>
+          )}
+          {data.promotionalPrice ? (
+            <>
+              <span className={styles.oldPriceValue}>
+                {formatPrice(data.fullPrice)}
+              </span>
+
+              <div className={styles.promoPriceWrapper}>
+                <DiscountIcon width={12} height={12} />
+                <span className={styles.promoPriceValue}>
+                  {formatPrice(data.promotionalPrice)}
+                </span>
+              </div>
+            </>
+          ) : (
+            <span className={styles.priceValue}>
+              {formatPrice(data.fullPrice)}
+            </span>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ src/components/catalogo/products-list/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./products-list.module.css";
+import { MenuCategory } from "@/types/products";
+import { ChevronDownIcon, DiscountIcon } from "@/assets/icons";
+import { ProductCard } from "../product-card";
+
+interface Props {
+  items: MenuCategory[];
+}
+
+export function ProductsList({ items }: Props) {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const toggleItem = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  return (
+    <div className={styles.container}>
+      {items.map((category, index) => (
+        <div key={category.id} className={styles.item}>
+          <button className={styles.header} onClick={() => toggleItem(index)}>
+            <div className={styles.contentHeaderWrapper}>
+              <div className={styles.titleHeaderWrapper}>
+                <p className={styles.titleHeader}>{category.category}</p>
+                {category.hasPromotionalPrice && (
+                  <DiscountIcon width={18} height={18} />
+                )}
+              </div>
+
+              {category.description && (
+                <p className={styles.descriptionHeader}>
+                  {category.description}
+                </p>
+              )}
+            </div>
+
+            <ChevronDownIcon
+              className={`${styles.icon} ${
+                openIndex === index ? styles.open : ""
+              }`}
+            />
+          </button>
+          {openIndex === index && (
+            <div className={styles.contentWrapper}>
+              {category.itens.map((item) => (
+                <ProductCard key={item.slug} data={item} />
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
