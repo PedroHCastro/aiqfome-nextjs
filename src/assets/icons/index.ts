eddcd64538ee5056b75ba348656e74e93b60d002
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 06:41:22 -0300

    feat: add chevron right icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,10 @@
+import ChevronRightIcon from "./chevron-right-icon.svg";
 import ChevronDownIcon from "./chevron-down-icon.svg";
 import DiscountIcon from "./discount-icon.svg";
 import HotIcon from "./hot-icon.svg";
 import VegIcon from "./veg-icon.svg";
 export {
+  ChevronRightIcon,
   ChevronDownIcon,
   DiscountIcon,
   HotIcon,
