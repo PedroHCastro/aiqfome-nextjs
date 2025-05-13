acea515330ae777759d3059ae0f0c8d0a44ada16
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 06:24:27 -0300

    feat: add icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,10 @@
+import ChevronDownIcon from "./chevron-down-icon.svg";
+import DiscountIcon from "./discount-icon.svg";
+import HotIcon from "./hot-icon.svg";
+import VegIcon from "./veg-icon.svg";
+export {
+  ChevronDownIcon,
+  DiscountIcon,
+  HotIcon,
+  VegIcon,
+};
