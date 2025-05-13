5d96c1dfd36a54f89067d030d3e2cd5282d4aa52
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 06:29:18 -0300

    feat: add product type

---
+++ src/types/products.ts
@@ -0,0 +1,17 @@
+export interface ProductItem {
+  name: string;
+  slug: string;
+  fullPrice: number;
+  promotionalPrice?: number;
+  description: string;
+  icon?: "veg" | "hot";
+  startingAt?: boolean;
+}
+
+export interface ProductsModel {
+  id: number;
+  category: string;
+  description: string;
+  hasPromotionalPrice?: boolean;
+  itens: ProductItem[];
+}
