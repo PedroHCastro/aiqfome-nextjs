3431771e8b319df2aebb672ee5b04920106dadbc
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 23:37:23 -0300

    refactor: improve product page

--- src/app/catalogo/[slug]/[product]/page.tsx
+++ src/app/catalogo/[slug]/[product]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 import { QuantitySelector, OptionGroup } from "@/components/product";
@@ -6,7 +9,194 @@ import { formatPrice } from "@/utils";
 
 import styles from "./product.module.css";
 
+const dataLayoutConfig = [
+  {
+    title: "qual o tamanho?",
+    description: "escolha 1",
+    type: "radio",
+    required: true,
+    items: [
+      {
+        label: "Medium",
+        price: 22.9,
+        promotionalPrice: 19.9,
+      },
+      { label: "Large", price: 28.9 },
+    ],
+  },
+  {
+    title: "acompanhamentos",
+    description: "escolha de 1 a 2",
+    min: 1,
+    max: 2,
+    type: "checkbox",
+    required: true,
+    items: [
+      { label: "Shoyu" },
+      { label: "Ginger" },
+      { label: "Wasabi" },
+      { label: "No sides" },
+    ],
+  },
+  {
+    title: "vai querer bebida?",
+    description: "escolha quantos quiser",
+    type: "quantity",
+    items: [
+      { label: "Coca-Cola", price: 5, extra: true },
+      { label: "Fanta Orange", price: 5, extra: true },
+      { label: "Guaraná Antarctica", price: 5, extra: true },
+      { label: "Prats Orange Juice", price: 6, extra: true },
+      { label: "Still Water", price: 3, extra: true },
+    ],
+  },
+  {
+    title: "precisa de talher?",
+    description: "escolha até 1",
+    type: "radio",
+    max: 1,
+    items: [
+      { label: "Chopsticks" },
+      { label: "Disposable fork and knife", price: 1, extra: true },
+    ],
+  },
+  {
+    title: "mais alguma coisa?",
+    description: "escolha até 2",
+    type: "checkbox",
+    max: 2,
+    items: [
+      { label: "Fortune cookie", price: 2, extra: true },
+      { label: "Spring roll", price: 8, extra: true },
+      { label: "Gyoza", price: 6, extra: true },
+    ],
+  },
+];
+
 export default function ProductPage() {
+  const [layoutConfig, setlayoutConfig] = useState();
+  const [currentItem, setCurrentItem] = useState({
+    id: 1,
+    name: "Ceviche de salmão",
+    price: 19.9,
+    quantity: 1,
+    items: [],
+  });
+  const [startingAtValue] = useState(currentItem.price);
+
+  const handleAddQuantity = () => {
+    setCurrentItem((prev) => ({
+      ...prev,
+      quantity: prev.quantity + 1,
+    }));
+  };
+  const handleremoveQuantity = () => {
+    setCurrentItem((prev) => ({
+      ...prev,
+      quantity: prev.quantity - 1,
+    }));
+  };
+
+  const handleAddItem = (option: any, configTitle: string, quantity = 1) => {
+    const isExtra = option.extra && option.prince;
+
+    const alreadyExists = currentItem.items.find(
+      (item) => item.label === option.label && item.configTitle === configTitle
+    );
+
+    let updatedItems;
+
+    if (alreadyExists) {
+      updatedItems = currentItem.items.map((item) => {
+        if (item.label === option.label && item.configTitle === configTitle) {
+          return { ...item, quantity };
+        }
+        return item;
+      });
+    } else {
+      const newItem = {
+        ...option,
+        configTitle,
+        quantity,
+      };
+      updatedItems = [...currentItem.items, newItem];
+    }
+
+    const baseItem = !isExtra
+      ? updatedItems.find(
+          (item) => item.configTitle === configTitle && !item.extra
+        )
+      : null;
+
+    const basePrice = baseItem
+      ? baseItem.price || startingAtValue
+      : startingAtValue;
+
+    const sumExtras = updatedItems
+      .filter((item) => item.extra)
+      .reduce((acc, item) => {
+        const itemPrice = item.promotionalPrice ?? item.price ?? 0;
+        const itemQuantity = item.quantity ?? 1;
+
+        return acc + itemPrice * itemQuantity;
+      }, 0);
+
+    console.log({ isExtra, basePrice, sumExtras });
+
+    const totalPrice = basePrice + sumExtras;
+
+    setCurrentItem({
+      ...currentItem,
+      price: totalPrice,
+      items: updatedItems,
+    });
+  };
+
+  const handleRemoveItem = (option: any, configTitle: string, quantity = 0) => {
+    setCurrentItem((prev) => {
+      const isExtra = option.extra;
+
+      let updatedItems;
+
+      if (quantity === 0) {
+        updatedItems = prev.items.filter(
+          (item) =>
+            !(item.label === option.label && item.configTitle === configTitle)
+        );
+      } else {
+        updatedItems = prev.items.map((item) => {
+          if (item.label === option.label && item.configTitle === configTitle) {
+            return { ...item, quantity };
+          }
+          return item;
+        });
+      }
+
+      const baseItem = updatedItems.find(
+        (item) => item.configTitle === configTitle && !item.extra
+      );
+
+      const basePrice = baseItem
+        ? baseItem.price || startingAtValue
+        : startingAtValue;
+
+      const sumExtras = updatedItems
+        .filter((item) => item.extra)
+        .reduce((acc, item) => {
+          const itemPrice = item.promotionalPrice ?? item.price ?? 0;
+          const itemQuantity = item.quantity ?? 1;
+
+          return acc + itemPrice * itemQuantity;
+        }, 0);
+
+      return {
+        ...prev,
+        price: basePrice + sumExtras,
+        items: updatedItems,
+      };
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Image
@@ -25,7 +215,9 @@ export default function ProductPage() {
         <h1 className={styles.title}>Ceviche de salmão</h1>
         <div className={styles.startingAtWrapper}>
           <span className={styles.startingAt}>a partir de</span>
-          <span className={styles.startingAtValue}>{formatPrice(19.9)}</span>
+          <span className={styles.startingAtValue}>
+            {formatPrice(startingAtValue)}
+          </span>
         </div>
         <span className={styles.description}>
           salmão temperado com limão, cebola e pimenta
@@ -36,90 +228,67 @@ export default function ProductPage() {
             <span className={styles.quantityLabel}>quantos?</span>
             <span className={styles.totalLabel}>
               total{" "}
-              <span className={styles.totalValue}>{formatPrice(29.9)}</span>
+              <span className={styles.totalValue}>
+                {formatPrice(currentItem.price * currentItem.quantity)}
+              </span>
             </span>
           </div>
 
-          <QuantityItem />
+          {currentItem && currentItem.quantity > 0 ? (
+            <QuantityItem
+              handleRemoveItem={handleremoveQuantity}
+              handleAddItem={handleAddQuantity}
+              quantity={currentItem.quantity}
+            />
+          ) : (
+            <button className={styles.addItemButon} onClick={handleAddQuantity}>
+              adicionar
+            </button>
+          )}
         </div>
       </div>
 
-      <div className={styles.contentWrapper}>
-        <OptionGroup
-          title="qual o tamanho?"
-          description="escolha 1"
-          type="radio"
-          required
-          options={[
-            {
-              label: "Medium",
-              description: "From R$ 22.90 to R$ 19.90",
-              highlight: true,
-            },
-            { label: "Large", description: "R$ 28.90" },
-          ]}
-        />
-      </div>
+      {dataLayoutConfig.map((layoutConfigItem) => {
+        if (layoutConfigItem.type === "quantity") {
+          return (
+            <div key={layoutConfigItem.title} className={styles.contentWrapper}>
+              <QuantitySelector
+                title={layoutConfigItem.title}
+                description={layoutConfigItem.description}
+                options={layoutConfigItem.items}
+                handleRemoveItem={handleRemoveItem}
+                handleAddItem={handleAddItem}
+              />
+            </div>
+          );
+        }
 
-      <div className={styles.contentWrapper}>
-        <OptionGroup
-          title="acompanhamentos"
-          description="escolha de 1 a 2"
-          type="checkbox"
-          required
-          min={1}
-          max={2}
-          options={[
-            { label: "Shoyu" },
-            { label: "Ginger" },
-            { label: "Wasabi" },
-            { label: "No sides" },
-          ]}
-        />
-      </div>
-      <div className={styles.contentWrapper}>
-        <QuantitySelector
-          title="vai querer bebida?"
-          description="escolha quantos quiser"
-          items={[
-            { label: "Coca-Cola", price: 5 },
-            { label: "Fanta Orange", price: 5 },
-            { label: "Guaraná Antarctica", price: 5 },
-            { label: "Prats Orange Juice", price: 6 },
-            { label: "Still Water", price: 3 },
-          ]}
-        />
-      </div>
-      <div className={styles.contentWrapper}>
-        <OptionGroup
-          title="precisa de talher?"
-          description="escolha até 1"
-          type="radio"
-          max={1}
-          options={[
-            { label: "Chopsticks" },
-            { label: "Disposable fork and knife", price: 1 },
-          ]}
-        />
-      </div>
-      <div className={styles.contentWrapper}>
-        <OptionGroup
-          title="mais alguma coisa?"
-          description="escolha até 2"
-          type="checkbox"
-          max={2}
-          options={[
-            { label: "Fortune cookie", price: 2 },
-            { label: "Spring roll", price: 8 },
-            { label: "Gyoza", price: 6 },
-          ]}
-        />
-      </div>
+        return (
+          <div key={layoutConfigItem.title} className={styles.contentWrapper}>
+            <OptionGroup
+              title={layoutConfigItem.title}
+              description={layoutConfigItem.description}
+              type={layoutConfigItem.type}
+              required={layoutConfigItem.required}
+              options={layoutConfigItem.items}
+              handleRemoveItem={handleRemoveItem}
+              handleAddItem={handleAddItem}
+              max={layoutConfigItem.max}
+              min={layoutConfigItem.min}
+            />
+          </div>
+        );
+      })}
+
       <div className={styles.contentWrapper}>
         <textarea
           className={styles.observationArea}
           placeholder="alguma observação do item? • opcional ex: tirar algum ingrediente, ponto do prato"
         />
+      </div>
+
+      <div className={styles.buttonTickerWrapper}>
+        <button className={styles.seeTicketButton}>ver ticket</button>
       </div>
     </div>
   );
