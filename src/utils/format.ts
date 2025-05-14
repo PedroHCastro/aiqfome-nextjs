bfc21f442d4754a8c7a6a65c7ab2bbdd2f90224f
Pedro Castro <pedro.castro@Pedros-iMac.local>
2025-05-13 23:30:16 -0300

    refactor: utils format price

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,2 +1,6 @@
-export const formatPrice = (price: number) =>
-  `R$ ${price.toFixed(2).replace(".", ",")}`;
+export const formatPrice = (price: number) => {
+  if (typeof price === "number")
+    return `R$ ${price.toFixed(2).replace(".", ",")}`;
+
+  return `R$ ${price}`;
+};
